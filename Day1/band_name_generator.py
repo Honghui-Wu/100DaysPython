cf68f8dc53bfdbd8037cd347156244a28b422d9d
Honghui-Wu <whh124@outlook.com>
2025-06-15 00:30:21 -0500

    Create band_name_generator.py

---
+++ Day1/band_name_generator.py
@@ -0,0 +1,7 @@
+print("Hello, this is a program to generate a Band Name!")
+
+city_name = input("What city did you grow up in?\n")
+
+pet_name = input("What is the name of your first pet?\n")
+
+print("The Band Name could be " + city_name + " " + pet_name)
